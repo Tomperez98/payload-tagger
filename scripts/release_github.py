ef83dcbc6101fd6484cd0effa28b5b62a99891d8
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2023-06-22 15:07:05 -0500

    Add release_github script

--- scripts/release_github.py
+++ scripts/release_github.py
@@ -1,45 +1,38 @@
-"""Release GitHub."""
+"""New release."""
 from __future__ import annotations
 
 import pathlib
-import re
-import sys
 import webbrowser
+from typing import Any
 from urllib.parse import urlencode
 
 import tomli
 
 
+def project_project(cwd: pathlib.Path) -> dict[str, Any]:
+    """Read `pyproject.toml` project."""
+    pyproject = tomli.loads(
+        cwd.joinpath(
+            "pyproject.toml",
+        ).read_text(encoding="utf-8"),
+    )
+
+    return pyproject["project"]
+
+
 def main() -> None:
-    """
-    Open a tab ready to review and approve for new release.
-
-    Based on https://github.com/pypa/hatch/blob/master/scripts/release_github.py
-    """
-    pkg_data = tomli.loads(
-        pathlib.Path("pyproject.toml").read_text(encoding="utf-8")
-    )
-    about_data = (pathlib.Path("ai_assistant") / "__about__.py").read_text(
-        encoding="utf-8",
-    )
-    search_result = re.search(r"__version__ = \"(.*?)\"", about_data)
-    if search_result is None:
-        sys.exit(1)
-
-    version = search_result.group(1)
+    """Prepare new release."""
+    cwd = pathlib.Path().cwd()
+    project = project_project(cwd=cwd)
+    version = project["version"]
+    source = project["urls"]["Source"]
     params = urlencode(
         query={
             "title": f"v{version}",
             "tag": f"v{version}",
         },
     )
-
-    webbrowser.open_new_tab(
-        url="{source}/releases/new?{params}".format(
-            source=pkg_data["project"]["urls"]["Source"],
-            params=params,
-        ),
-    )
+    webbrowser.open_new_tab(url=f"{source}/releases/new?{params}")
 
 
 if __name__ == "__main__":
