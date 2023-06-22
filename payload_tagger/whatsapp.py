a3d17b22ba93e9791101788a6b8e456b502b3a20
Tomas Perez Alvarez <tomasperezalvarez@gmail.com>
2023-06-22 15:34:10 -0500

    More efficient algorithm

--- payload_tagger/whatsapp.py
+++ payload_tagger/whatsapp.py
@@ -35,10 +35,11 @@ def _has_type(payload_changes: dict[str, Any]) -> bool:
 
 
 def _is_text_message(payload_changes: dict[str, Any]) -> bool:
+    msgs: dict[str, Any] = payload_changes["value"]["messages"][0]
     if (
-        "referral" not in payload_changes["value"]["messages"][0]
-        and "context" not in payload_changes["value"]["messages"][0]
-        and payload_changes["value"]["messages"][0]["type"] == "text"
+        "referral" not in msgs
+        and "context" not in msgs
+        and msgs["type"] == "text"
     ):
         return True
     return False
@@ -79,10 +80,10 @@ def _is_received_callback_from_quick_reply_button(
 def _is_received_answer_from_list_message(
     payload_changes: dict[str, Any],
 ) -> bool:
+    msgs: dict[str, Any] = payload_changes["value"]["messages"][0]
     if (
-        payload_changes["value"]["messages"][0]["type"] == "interactive"
-        and payload_changes["value"]["messages"][0]["interactive"]["type"]
-        == "list_reply"
+        msgs["type"] == "interactive"
+        and msgs["interactive"]["type"] == "list_reply"
     ):
         return True
     return False
@@ -91,10 +92,10 @@ def _is_received_answer_from_list_message(
 def _is_received_answer_to_reply_button(
     payload_changes: dict[str, Any],
 ) -> bool:
+    msgs: dict[str, Any] = payload_changes["value"]["messages"][0]
     if (
-        payload_changes["value"]["messages"][0]["type"] == "interactive"
-        and payload_changes["value"]["messages"][0]["interactive"]["type"]
-        == "button_reply"
+        msgs["type"] == "interactive"
+        and msgs["interactive"]["type"] == "button_reply"
     ):
         return True
     return False
@@ -115,12 +116,49 @@ def _message_changes(
     return Ok(changes[0])
 
 
-def identify_payload(  # noqa: C901, PLR0912
+def _identify_payload_no_type(
+    payload_changes: dict[str, Any],
+) -> SupportedWebhooks | None:
+    identified: SupportedWebhooks
+
+    if _is_location_message(payload_changes=payload_changes):
+        identified = "location-message"
+    elif _is_contact_message(payload_changes=payload_changes):
+        identified = "contact-message"
+    else:
+        return None
+    return identified
+
+
+def _identify_core_msgs(
+    payload_changes: dict[str, Any],
+) -> SupportedWebhooks | None:
+    identified: SupportedWebhooks
+    if _is_text_message(payload_changes=payload_changes):
+        identified = "text-message"
+    elif _is_reaction_message(payload_changes=payload_changes):
+        identified = "reaction-message"
+    elif _is_media_message(payload_changes=payload_changes):
+        identified = "media-message"
+    elif _is_received_callback_from_quick_reply_button(
+        payload_changes=payload_changes,
+    ):
+        identified = "received-callback-from-quick-reply-button"
+    elif _is_received_answer_from_list_message(
+        payload_changes=payload_changes,
+    ):
+        identified = "received-answer-from-list-message"
+    elif _is_received_answer_to_reply_button(payload_changes=payload_changes):
+        identified = "received-answer-to-reply-button"
+    else:
+        return None
+    return identified
+
+
+def identify_payload(
     payload: dict[str, Any],
 ) -> Result[SupportedWebhooks, errors.NotIdentifiedPayloadError]:
     """Identify Whatsapp payload."""
-    identified: SupportedWebhooks
-
     changes_retrieved = _message_changes(payload=payload)
     if not isinstance(changes_retrieved, Ok):
         return changes_retrieved
@@ -131,31 +169,12 @@ def identify_payload(  # noqa: C901, PLR0912
         return Err(errors.NotIdentifiedPayloadError(payload=payload))
 
     if not _has_type(payload_changes=changes):
-        if _is_location_message(payload_changes=changes):
-            identified = "location-message"
-        elif _is_contact_message(payload_changes=changes):
-            identified = "contact-message"
-        else:
-            return Err(errors.NotIdentifiedPayloadError(payload=payload))
-        return Ok(identified)
-
-    if _is_text_message(payload_changes=changes):
-        identified = "text-message"
-    elif _is_reaction_message(payload_changes=changes):
-        identified = "reaction-message"
-    elif _is_media_message(payload_changes=changes):
-        identified = "media-message"
-    elif _is_received_callback_from_quick_reply_button(
-        payload_changes=changes,
-    ):
-        identified = "received-callback-from-quick-reply-button"
-    elif _is_received_answer_from_list_message(
-        payload_changes=changes,
-    ):
-        identified = "received-answer-from-list-message"
-    elif _is_received_answer_to_reply_button(payload_changes=changes):
-        identified = "received-answer-to-reply-button"
-    else:
+        identity = _identify_payload_no_type(payload_changes=changes)
+        if identity:
+            return Ok(identity)
         return Err(errors.NotIdentifiedPayloadError(payload=payload))
 
-    return Ok(identified)
+    identity = _identify_core_msgs(payload_changes=changes)
+    if identity:
+        return Ok(identity)
+    return Err(errors.NotIdentifiedPayloadError(payload=payload))
